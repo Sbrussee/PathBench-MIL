e8097268d50c232fd7a7e024969e9daf22e736f2
Sbrussee <Siemenbrussee@hotmail.com>
2024-04-22 14:43:00 +0200

    fixed vis

--- pathbench/visualization/visualization.py
+++ pathbench/visualization/visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 
 def visualize_activations(features, config, dataset, save_string):
-    os.makedirs(f"{config['experiment']['name']}/visualizations", exist_ok=True)
+    os.makedirs(f"{config['experiment']['project_name']}/visualizations", exist_ok=True)
     slide_map = features.map_activations(
         n_neighbors=10,
         min_dist=0.2
@@ -11,5 +11,5 @@ def visualize_activations(features, config, dataset, save_string):
     labels, unique_labels = dataset.labels('category', format='name')
     slide_map.label_by_slide(labels)
     slide_map.plot(s=10)
-    plt.savefig(f"{config['experiment']['name']}/visualizations/{save_string}.png")
+    plt.savefig(f"{config['experiment']['project_name']}/visualizations/{save_string}.png")
     plt.close()
