b7eabeba34fdf6276798670e94290af9489f548f
Sbrussee <Siemenbrussee@hotmail.com>
2024-04-25 11:40:50 +0200

    test

--- pathbench/benchmarking/benchmark.py
+++ pathbench/benchmarking/benchmark.py
@@ -144,10 +144,10 @@ def benchmark(config, project):
             val_df = val_df.append(val_dict, ignore_index=True)
             #Test the trained model
             test_result = project.evaluate_mil(
-                model= f"experiments/{config['experiment']['project_name']}/mil/{save_string}_{index}/{number}-{save_string}_{index}",
+                model= f"{config['experiment']['project_name']}/mil/{save_string}_{index}/{number}-{save_string}_{index}",
                 outcomes='category',
                 dataset=test_set,
-                bags=f"experiments/{config['experiment']['project_name']}/bags/{save_string}",
+                bags=f"{config['experiment']['project_name']}/bags/{save_string}",
                 config=mil_conf,
             )   
 
