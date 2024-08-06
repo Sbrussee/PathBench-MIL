3f0d9075ac3617b1c8b4471304195c2347eb27f9
Brussee <sbrussee@res-hpc-lo02.researchlumc.nl>
2024-08-06 13:21:40 +0200

    removed unnessary files

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from pathbench.experiment.experiment import Experiment
+import argparse
+
+# Add arguments
+parser = argparse.ArgumentParser(description='Run PathDev experiment')
+parser.add_argument('--config', type=str, default='conf.yaml', help='Path to the configuration file')
+args = parser.parse_args()
 
 def main():
     """"
     Main function to run the experiment
     """
     # Create an instance of the Experiment class
-    experiment = Experiment('conf.yaml')
+    experiment = Experiment(args.config)
     experiment.run()
 
 if __name__ == "__main__":
